5b55212fc46799c4fa8a8b1dc40467bad7f67777
Reetpriye <reetpriye@gmail.com>
2020-08-24 20:30:39 +0530

    Add STL Notes

---
+++ 24 STL/00ReadMeFirst.cpp
@@ -0,0 +1,11 @@
+// Author: REET
+
+/*
+==================NOTE==================
+This is just for reference of C++
+Standard Template Library(STL).
+STL basically provides four components
+which ease the CP journey.
+The components are: algorithms,containers
+=========================================
+*/
